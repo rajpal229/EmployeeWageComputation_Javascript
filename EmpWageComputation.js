38de2f544a27e5a658930b38ebd1b9df3bd44f3b
rajpal229 <rajpalrawat229@gmail.com>
2023-01-19 22:54:03 +0530

    Added: UC2_DailyWage

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -1,10 +1,49 @@
-const is_absent = 0;
-let empcheck=Math.floor(Math.random() * 10%2);
-if(empcheck == is_absent)
-{
-    console.log("Employee is absent")
+class UC1_Attendence{
+    attendence(){
+        const is_absent = 0;
+        let empcheck=Math.floor(Math.random() * 10%2);
+        if(empcheck == is_absent)
+        {
+            console.log("UC1-Employee is absent")
+            return 0;
+        }
+        else
+        {
+            console.log("UC1-Employee is present")
+            return 1;
+        }
+    }
 }
-else
-{
-    console.log("Employee is present")
+class UC2_Full_or_Part_Time{
+    Time(){
+        var obj = new UC1_Attendence();
+        var att = obj.attendence();
+        const Is_Part_Time=1;
+        const Is_Full_Time=2;
+        const part_time_hours=4;
+        const full_time_hours=8;
+        const wage_per_hour=20;
+        let wage = 0;
+        let emphrs=0;
+        if(att == 1)
+        {
+            let empcheck1=Math.floor(((Math.random() * 10 % 2) + 1));
+        switch(empcheck1)
+        {
+            case Is_Part_Time:
+                emphrs=part_time_hours;
+                console.log("Present Part Time")
+                break;
+            case Is_Full_Time:
+            emphrs=full_time_hours;
+            console.log("Present Full Time")
+                break;
+        }
+        wage = emphrs * wage_per_hour;
+        console.log("Wage of the day is: " + wage)
+        }
+    }
 }
+
+var obj1 = new UC2_Full_or_Part_Time();
+obj1.Time()
