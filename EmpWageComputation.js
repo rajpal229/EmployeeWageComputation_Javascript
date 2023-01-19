8edcd2fbb3227034c89dcb1dd8da2af863ff2721
rajpal229 <rajpalrawat229@gmail.com>
2023-01-19 22:21:55 +0530

    Addded: UC1_Attendence

---
+++ EmpWageComputation.js
@@ -0,0 +1,10 @@
+const is_absent = 0;
+let empcheck=Math.floor(Math.random() * 10%2);
+if(empcheck == is_absent)
+{
+    console.log("Employee is absent")
+}
+else
+{
+    console.log("Employee is present")
+}
