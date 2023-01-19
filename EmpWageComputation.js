deaa14c4af625a7ff455a2bb0bc94ab9068f288d
rajpal229 <rajpalrawat229@gmail.com>
2023-01-19 23:09:05 +0530

    Added: UC4_Monthly_Salary

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -1,5 +1,7 @@
-class UC1_Attendence{
-    attendence(){
+class UC1_Attendence
+{
+    attendence()
+    {
         const is_absent = 0;
         let empcheck=Math.floor(Math.random() * 10%2);
         if(empcheck == is_absent)
@@ -14,8 +16,10 @@ class UC1_Attendence{
         }
     }
 }
-class UC2_Full_or_Part_Time{
-    Time(){
+class UC2_Full_or_Part_Time
+{
+    Wage()
+    {
         var obj = new UC1_Attendence();
         var att = obj.attendence();
         const Is_Part_Time=1;
@@ -23,27 +27,34 @@ class UC2_Full_or_Part_Time{
         const part_time_hours=4;
         const full_time_hours=8;
         const wage_per_hour=20;
+        const DaysInMonths = 20;
+        let monthlywage = 0;
         let wage = 0;
         let emphrs=0;
-        if(att == 1)
+        for (var i = 1; i <=DaysInMonths; i++)
         {
-            let empcheck1=Math.floor(((Math.random() * 10 % 2) + 1));
-        switch(empcheck1)
-        {
-            case Is_Part_Time:
-                emphrs=part_time_hours;
-                console.log("Present Part Time")
-                break;
-            case Is_Full_Time:
-            emphrs=full_time_hours;
-            console.log("Present Full Time")
-                break;
+            if(att == 1)
+            {
+                let empcheck1=Math.floor(((Math.random() * 10 % 2) + 1));
+            switch(empcheck1)
+            {
+                case Is_Part_Time:
+                    emphrs=part_time_hours;
+                    console.log("Present Part Time")
+                    break;
+                case Is_Full_Time:
+                emphrs=full_time_hours;
+                console.log("Present Full Time")
+                    break;
+            }
+            }
+            wage = emphrs * wage_per_hour;
+            console.log("Wage of the day is: " + wage)
+            monthlywage = monthlywage + wage;
         }
-        wage = emphrs * wage_per_hour;
-        console.log("Wage of the day is: " + wage)
-        }
+        console.log("Monthly Wage is: " + monthlywage)
     }
 }
 
 var obj1 = new UC2_Full_or_Part_Time();
-obj1.Time()
+obj1.Wage()
